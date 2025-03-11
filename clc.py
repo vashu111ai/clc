7c75b111bf455255db2cd26de83c6081369942f3
vashu111ai <vidhusai41414@gmail.com>
2025-03-11 09:38:29 +0530

    Add files via upload

---
+++ clc.py
@@ -0,0 +1,35 @@
+def add():
+    a = int(input("first value."))
+    b = int(input("second value.."))
+    print("the add of", a ,"and", b ,"is" ,a+b)
+def sub():
+    a = int(input("first value."))
+    b = int(input("second value.."))
+    print("the sub of", a ,"and", b ,"is" ,a-b)
+
+def mul():
+    a = int(input("first value."))
+    b = int(input("second value.."))
+    print("the multiple of", a ,"and", b ,"is" ,a*b)
+def div():
+    a = int(input("first value."))
+    b = int(input("second value.."))
+    print("the divide of", a ,"and", b ,"is" ,a/b)
+def power():
+    a = int(input("Give a No for power of"))
+    b = int(input("power value"))
+    print(a**b)
+print("this is vahsu calc in python")
+c = input("what you want to do like + - * / PO")
+if c == "+":
+    add()
+elif c == "-":
+    sub()
+elif c == "*":
+    mul()
+elif c == "/":
+    div()
+elif c == "PO":
+    power()
+else:
+    print("wrong input please choose between + - * / only")
